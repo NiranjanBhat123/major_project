0917073044e42ae222b06318e3039c0150e47f79
Vighnesh-bhat <139466978+Vighnesh-bhat@users.noreply.github.com>
2024-11-15 17:39:20 +0530

    admin.py for orders

--- backend/server/orders/admin.py
+++ backend/server/orders/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Orders, OrderItems, OrderStatusHistory
 
-# Register your models here.
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'provider', 'service', 'status', 'ordered_on', 'scheduled_on', 'total_price')
+    list_filter = ('status', 'ordered_on', 'scheduled_on')
+    search_fields = ('id', 'user__name', 'provider__name', 'service__name')  
+    ordering = ('-ordered_on',)
+    readonly_fields = ('id', 'ordered_on')
+
+@admin.register(OrderItems)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'subservice', 'price')
+    list_filter = ('subservice',)
+    search_fields = ('id', 'order__id', 'subservice__name')  
+    ordering = ('order',)
+
+@admin.register(OrderStatusHistory)
+class OrderStatusHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'status', 'changed_on')
+    list_filter = ('status', 'changed_on')
+    search_fields = ('id', 'order__id', 'status')
+    ordering = ('-changed_on',)
+from django.contrib import admin
+from .models import Orders, OrderItems, OrderStatusHistory
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'provider', 'service', 'status', 'ordered_on', 'scheduled_on', 'total_price')
+    list_filter = ('status', 'ordered_on', 'scheduled_on')
+    search_fields = ('id', 'user__name', 'provider__name', 'service__name')  
+    ordering = ('-ordered_on',)
+    readonly_fields = ('id', 'ordered_on')
+
+@admin.register(OrderItems)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'subservice', 'price')
+    list_filter = ('subservice',)
+    search_fields = ('id', 'order__id', 'subservice__name')  
+    ordering = ('order',)
+
+@admin.register(OrderStatusHistory)
+class OrderStatusHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'status', 'changed_on')
+    list_filter = ('status', 'changed_on')
+    search_fields = ('id', 'order__id', 'status')
+    ordering = ('-changed_on',)
